337cd9785aeeba35939818cbe5766b870fbb4731
Vladimirshubyn <vladimirshubyn@gmail.com>
2020-10-24 19:59:42 +0700

    Вторая попытка

--- js/script.js
+++ js/script.js
@@ -4,25 +4,27 @@ const form = document.querySelector(".search-form");
 const arrival = popup.querySelector("[name=start-date]");
 const departure = popup.querySelector("[name=end-date]");
 
+popup.classList.remove("modal-show");
+
 link.addEventListener("click", function (evt) {
-    evt.preventDefault();
+  evt.preventDefault();
     popup.classList.toggle("modal-show");
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-        if (popup.classList.contains("modal-show")) {
-            popup.classList.remove("modal-show");
-            popup.classList.remove("modal-error");
-        }
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains("modal-show")) {
+      popup.classList.remove("modal-show");
+      popup.classList.remove("modal-error");
     }
+  }
 });
 
 form.addEventListener("submit", function (evt) {
-    if (!arrival.value || !departure.value) {
-        evt.preventDefault();
-        popup.classList.remove("modal-error");
-        popup.offsetWidth = popup.offsetWidth;
-        popup.classList.add("modal-error");
-    }
+  if (!arrival.value || !departure.value) {
+    evt.preventDefault();
+    popup.classList.remove("modal-error");
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add("modal-error");
+  }
 });
